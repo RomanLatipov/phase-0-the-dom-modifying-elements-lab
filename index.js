b3043b478eceb72d326f618800f94fbba7695df7
RomanLatipov <romanlatipov@gmail.com>
2024-01-29 01:51:55 -0500

    completed dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementsByTagName("main")[0];
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Roman is the champion";
